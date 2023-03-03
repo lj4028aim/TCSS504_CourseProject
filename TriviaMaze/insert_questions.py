08f149fbaa0d68a49b1aad8ab2dd626826665086
Candice Long <candice@Candices-MacBook-Pro.local>
2023-03-03 01:41:01 -0800

    incorporate display question

--- TriviaMaze/insert_questions.py
+++ TriviaMaze/insert_questions.py
@@ -1,7 +1,21 @@
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
+import os
 
+
+
+# the relative file path
+path = 'db/TriviaMaze.db'
+csv_path = 'TriviaMazeQuestion/csv_version/'
+# get the path to the directory this script is in
+script_dir = os.path.dirname(__file__)
+# add the relative path to the database file from there
+db_path = os.path.join(script_dir, path)
+# add the relative path to input question csv file
+q_path = os.path.join(script_dir, csv_path)
+# make sure the path exists and if not create it
+os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
 def create_connection(db_file):
     """
@@ -34,31 +48,31 @@ def main():
      Inserting values stored in csv files into database with panda package
      :return:
      """
-    database = "C:/Users/Ji/PycharmProjects/504pythonProject/TCSS504_CourseProject/TriviaMaze/TriviaMaze.db"
-    question_path = "C:/Users/Ji/PycharmProjects/504pythonProject/TCSS504_CourseProject/TriviaMaze/TriviaMazeQuestion/csv_version/"
+    database = db_path
+    question_path = q_path
     conn = create_connection(database)
 
     question_list = ["animals.csv",
-                     "brain-teasers.csv",
-                     "celebrities.csv",
-                     "entertainment.csv",
-                     "for-kids.csv",
-                     "general.csv",
-                     "geography.csv",
-                     "history.csv",
-                     "hobbies.csv",
-                     "humanities.csv",
-                     "literature.csv",
-                     "movies.csv",
-                     "music.csv",
-                     "newest.csv",
-                     "people.csv",
-                     "rated.csv",
-                     "religion-faith.csv",
-                     "science-technology.csv",
-                     "sports.csv",
-                     "television.csv",
-                     "video-games.csv",
+                     # "brain-teasers.csv",
+                     # "celebrities.csv",
+                     # "entertainment.csv",
+                     # "for-kids.csv",
+                     # "general.csv",
+                     # "geography.csv",
+                     # "history.csv",
+                     # "hobbies.csv",
+                     # "humanities.csv",
+                     # "literature.csv",
+                     # "movies.csv",
+                     # "music.csv",
+                     # "newest.csv",
+                     # "people.csv",
+                     # "rated.csv",
+                     # "religion-faith.csv",
+                     # "science-technology.csv",
+                     # "sports.csv",
+                     # "television.csv",
+                     # "video-games.csv",
                      "world.csv"]
     fields = ["Questions", "answer_A", "answer_B", "answer_C", "answer_D", "Correct_answer"]
     for i in range(len(question_list)):
--- TriviaMaze/select_questions.py
+++ TriviaMaze/select_questions.py
@@ -1,6 +1,18 @@
 import sqlite3
 from sqlite3 import Error
 import random
+import os
+
+
+# the relative file path
+path = 'db/TriviaMaze.db'
+
+# get the path to the directory this script is in
+scriptdir = os.path.dirname(__file__)
+# add the relative path to the database file from there
+db_path = os.path.join(scriptdir, path)
+
+
 
 
 def create_connection(db_file):
@@ -19,42 +31,30 @@ def create_connection(db_file):
     return conn
 
 
-def get_questions():
+def get_questions(num_questions_expect):
     """
     Connecting to database and fetch certain amount of questions. Storing all fetched questions into
-    a dictionary.
-    :return: a dictionary which hold all questions and answers
+    :param num_questions_expect: number of questions expected
+    :return: a list which hold all questions. Each question and its associated answers is in dictionary format.
     """
     try:
-        database = "C:/Users/Ji/PycharmProjects/504pythonProject/TCSS504_CourseProject/TriviaMaze/TriviaMaze.db"
 
-        conn = create_connection(database)
+        conn = create_connection(db_path)
 
         cur = conn.cursor()
         cur.execute("SELECT *  FROM Questions")
-        num_question = random.randrange(10, 50)
-        rows = cur.fetchmany(num_question)
+        rows = cur.fetchall()
 
-        question = []
-        dist1 = []
-        dist2 = []
-        dist3 = []
-        dist4 = []
-        key = []
-        for row in rows:
-            question.append(str(row[0]).strip())
-            dist1.append(str(row[1]))
-            dist2.append(str(row[2]))
-            dist3.append(str(row[3]))
-            dist4.append(str(row[4]))
-            key.append((str(row[5])))
-
-        questions = {"question": question,
-                     "A": dist1,
-                     "B": dist2,
-                     "C": dist3,
-                     "D": dist4,
-                     "correct_answer": key}
+        questions = []
+        num_questions = gen_num_questions(num_questions_expect)
+        for i in num_questions:
+            question = {"question": str(rows[i][0]).strip(),
+                        "A": str(rows[i][1]),
+                        "B": str(rows[i][2]),
+                        "C": str(rows[i][3]),
+                        "D": str(rows[i][4]),
+                        "correct_answer": str(rows[i][5])}
+            questions.append(question)
         cur.close()
         return questions
     except sqlite3.Error as error:
@@ -63,6 +63,28 @@ def get_questions():
         if conn:
             conn.close()
 
+
+def gen_num_questions(num_questions_expect):
+    """
+    Generate a list holds all unique randomly generated number
+    :return: a list which is composed of unique question index
+    """
+    total_num_questions = 49825  # number of questions stored in database
+    num_questions = []
+    while True:
+        num = random.randrange(0, total_num_questions)
+        num_questions.append(num)
+        if len(num_questions) == 1:
+            pass
+        else:
+            for i in range(len(num_questions) - 1):
+                if num == num_questions[i]:
+                    num_questions.pop()
+                    pass
+        if len(num_questions) == num_questions_expect:
+            break
+
+    return num_questions
 
 def get_answer(questions):
     """
@@ -74,14 +96,15 @@ def get_answer(questions):
 
 
 if __name__ == "__main__":
-    q = get_questions()
-    answer = get_answer(q)
-    num_questions = len(q["question"])
-    print(f"There are totally {num_questions} questions selected: \n")
-    for i in range(len(q["question"])):
-        print(q["question"][i])
-        print(q["A"][i], end=", ")
-        print(q["B"][i], end=", ")
-        print(q["C"][i], end=", ")
-        print(q["D"][i])
-        print(get_answer(q)[i], "\n")
+    pass
+    # q = get_questions()
+    # answer = get_answer(q)
+    # num_questions = len(q["question"])
+    # print(f"There are totally {num_questions} questions selected: \n")
+    # for i in range(len(q["question"])):
+    #     print(q["question"][i])
+    #     print(q["A"][i], end=", ")
+    #     print(q["B"][i], end=", ")
+    #     print(q["C"][i], end=", ")
+    #     print(q["D"][i])
+    #     print(get_answer(q)[i], "\n")
