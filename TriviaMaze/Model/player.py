2cdd7806d61ba79f19d75b873e341ecc4ecc0be0
jesse <itsjessef@gmail.com>
2023-03-16 17:27:22 -0700

    remove duplicate player clas file and update player class to keep neccessary methods

--- TriviaMaze/Model/player.py
+++ TriviaMaze/Model/player.py
@@ -1,11 +1,16 @@
 class Player:
 
     def __init__(self, maze, name="monster", golden_key=1):
+        """Initialize a new instance of Player"""
         self.name = str(name)
         self.maze = maze
         self.coordinates = [1, 1]
         self.golden_key = int(golden_key)
         self.is_golden_key = False
+
+    def __repr__(self):
+        """Returns string representation of Player object"""
+        return f"name: {self.name}, # of golden keys: {self.golden_key}, is a golden key: {self.is_golden_key}"
 
     def move_player(self, x, y):
         updated_x = self.coordinates[0] + x
@@ -19,10 +24,6 @@ class Player:
         self.golden_key = 1
         self.is_golden_key = False
 
-    def get_name(self):
-        pass
-
-
     def get_golden_key(self):
         """
         Returns the number of golden keys
@@ -30,11 +31,11 @@ class Player:
         """
         return self.golden_key
 
-    def set_golden_key(self):
-        pass
-
     def get_is_golden_key(self):
-        """Returns True when there is permission to use golden key otherwise False"""
+        """
+        Returns True when there is permission to use golden key otherwise False
+        :return: boolean, there is permission to use a golden key
+        """
         return self.is_golden_key
 
     def set_is_golden_key(self, is_golden_key):
